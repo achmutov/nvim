28d375f5ad8da9c6c50289883f996892cd2b7d95
Andrew Chmutov <ulman.andre@gmail.com>
2024-01-02 22:46:11 +0100

    Fix transparency

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,11 +2,11 @@ function ColorMyPencils(color)
 	color = color or "nordic"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    -- vim.cmd [[
-    --     highlight SignColumn guibg=NONE
-    -- ]]
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd [[
+        highlight SignColumn guibg=NONE
+    ]]
     vim.api.nvim_command('hi Visual term=reverse cterm=reverse guibg=Black')
 	
 end
