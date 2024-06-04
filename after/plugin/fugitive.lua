1e290c0a8b87a89272b5db51423a62932d6ae464
Andrew Chmutov <ulman.andre@gmail.com>
2024-06-04 14:01:09 +0200

    Fix git commit

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>tB", ":Git blame<CR>");
-vim.keymap.set("n", "<leader>gc", ':Git commit ""<Left>');
+vim.keymap.set("n", "<leader>gc", ':Git commit -m ""<Left>');
