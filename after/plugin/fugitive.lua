35bab59ec965079a8565ec8450d02761fb4fda5b
Andrew Chmutov <ulman.andre@gmail.com>
2024-06-04 14:00:49 +0200

    Git blame and git commit

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>tB", ":Git blame<CR>");
+vim.keymap.set("n", "<leader>gc", ':Git commit ""<Left>');
--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,4 +1,16 @@
-require('gitsigns').setup()
+require('gitsigns').setup {
+    on_attach = function (buffnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = buffnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+    end
+}
 
 vim.cmd [[
     highlight GitSignsAdd guibg=NONE
