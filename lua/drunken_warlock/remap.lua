34800067b3ccae1c219da038aa59a3d03590e010
Andrew Chmutov <ulman.andre@gmail.com>
2024-07-22 23:23:53 +0200

    Minor changes]

--- lua/drunken_warlock/remap.lua
+++ lua/drunken_warlock/remap.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- move lines with vscode-ish style
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv")
 
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
 
