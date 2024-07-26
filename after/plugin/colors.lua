4b17d9fe6c55833beb6b8f0ac20e928cb1779f73
Andrew Chmutov <ulman.andre@gmail.com>
2024-07-26 03:01:43 +0200

    colors for highlighting

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -9,7 +9,7 @@ function ColorMyPencils(color)
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.cmd [[ highlight SignColumn guibg=NONE ]]
-    vim.api.nvim_command('hi Visual term=reverse cterm=reverse gui=reverse')
+    vim.api.nvim_command('hi Visual term=reverse cterm=reverse guibg=#555555 guifg=#FFFFFF')
 
 end
 
