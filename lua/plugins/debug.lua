1bae0e573f3550a0abd04f025b598f4cabe1fb9d
Andrew Chmutov <ulman.andre@gmail.com>
2024-09-23 01:43:44 +0200

    plugins: debug: Add dap support
    Signed-off-by: Andrew Chmutov <ulman.andre@gmail.com>

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,12 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+        "nvim-neotest/nvim-nio",
+        "williamboman/mason.nvim",
+    },
+    config = function()
+        require("config.debug")
+    end
+}
