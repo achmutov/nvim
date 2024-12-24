c4f71b1aa628c6a280d320a72ab1b09b1e1461d6
Andrew Chmutov <ulman.andre@gmail.com>
2024-12-24 15:14:18 +0100

    config: Quickfix to Telescope promotion
    Signed-off-by: Andrew Chmutov <ulman.andre@gmail.com>

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,7 +1,12 @@
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>vrr", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>vri", builtin.lsp_implementations, {})
+vim.keymap.set("n", "<leader>vdi", builtin.diagnostics, {})
