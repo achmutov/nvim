38286959d8cb46c9b0a530eeb1bab68b920405e0
Andrew Chmutov <ulman.andre@gmail.com>
2025-01-11 03:32:15 +0100

    fix(gruvbox): disable
    Signed-off-by: Andrew Chmutov <ulman.andre@gmail.com>

--- lua/config/gruvbox.lua
+++ lua/config/gruvbox.lua
@@ -2,5 +2,5 @@ require("gruvbox").setup {
     transparent_mode = true,
 }
 
-vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("gruvbox")
 vim.api.nvim_set_hl(0, "Visual", { bg = "#555555", fg = "#FFFFFF" })
