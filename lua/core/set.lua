b22d69f227f2df66735be4426b6add209f9463e5
Andrew Chmutov <ulman.andre@gmail.com>
2025-03-17 14:18:48 +0100

    feat: distinguish docker compose filetype

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -56,3 +56,12 @@ function trail()
         vim.cmd("noh")
     end)
 end
+
+vim.filetype.add({
+    pattern = {
+        ["docker-compose%.yml"] = "yaml.docker-compose",
+        ["docker-compose%.yaml"] = "yaml.docker-compose",
+        ["compose%.yml"] = "yaml.docker-compose",
+        ["compose%.yaml"] = "yaml.docker-compose",
+    },
+})
