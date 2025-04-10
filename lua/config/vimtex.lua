565dbec3ae242a5ae604c6ec8ba6c70b37799426
Andrew Chmutov <ulman.andre@gmail.com>
2025-04-10 10:36:10 +0200

    feat: Vimtext keymaps

--- lua/config/vimtex.lua
+++ lua/config/vimtex.lua
@@ -1,2 +1,5 @@
 vim.g.vimtex_view_general_viewer = "zathura"
 vim.g.vimtex_view_method = "zathura"
+
+vim.keymap.set("n", "<leader>lc", ":VimtexCompile<CR>")
+vim.keymap.set("n", "<leader>lv", ":VimtexView<CR>")
