e774d3fe4b9d29f55f537caecee7ee830fd80c54
Andrew Chmutov <ulman.andre@gmail.com>
2025-06-08 01:01:47 +0200

    feat: save lazy-lock.json in state if NixOS

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -14,5 +14,47 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local function isNixos()
+    local file = io.open("/etc/os-release")
+    if not file then
+        return false
+    end
+
+    local match = false
+    for line in file:lines() do
+        match = string.match(line, "^ID=nixos$")
+        if match then
+            break
+        end
+    end
+
+    file:close()
+    return match
+end
+
+local function copyFile(src, dst)
+    local srcFile = io.open(src, "r")
+    if not srcFile then
+        return false
+    end
+
+    local dstFile = io.open(dst, "w")
+    if dstFile then
+        return true
+    end
+
+    dstFile:write(srcFile:read())
+    return srcFile:close() and dstFile:close()
+end
+
+local lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json"
+if isNixos() then
+    local newLockfile = vim.fn.stdpath("state") .. "/lazy-lock.json"
+    assert(copyFile(lockfile, newLockfile))
+end
+
 -- Setup lazy.nvim
-require("lazy").setup({ spec = { { import = "plugins" } } })
+require("lazy").setup({
+    lockfile,
+    spec = { { import = "plugins" } },
+})
