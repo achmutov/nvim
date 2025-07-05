2ff586d36e3376b330d13c67137519fac68d449d
Andrew Chmutov <achmutov@proton.me>
2025-07-05 18:59:36 +0200

    feat: dropbar

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "Bekaboo/dropbar.nvim",
+        -- optional, but required for fuzzy finder support
+        dependencies = {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
+        config = function()
+            require("config.dropbar")
+        end,
+    },
+}
