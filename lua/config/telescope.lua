618cdda20464cbcb848746501687a799e6c8962f
Andrew Chmutov <achmutov@proton.me>
2025-11-02 21:32:28 +0100

    telescope: add live_grep

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -6,15 +6,13 @@ vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 
 -- Text
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>ps", function()
+vim.keymap.set("n", "<leader>pw", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
 
-local function grep_string()
-    builtin.grep_string({ function() end })
-end
-vim.keymap.set("n", "<leader>w", grep_string)
-vim.keymap.set("v", "<leader>w", grep_string)
+vim.keymap.set("n", "<leader>w", builtin.grep_string, {})
+vim.keymap.set("v", "<leader>w", builtin.grep_string, {})
 
 -- LSP
 vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
