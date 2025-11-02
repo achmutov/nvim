69e96591d3ef190cf58c94823de30c96cbdc6dad
Andrew Chmutov <achmutov@proton.me>
2025-11-02 22:02:43 +0100

    fix: tweak markdown renderer

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,6 +4,10 @@ return {
     build = "cd app && npm install",
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_auto_close = 0
+        vim.g.mkdp_preview_options = {
+            disable_sync_scroll = 1,
+        }
     end,
     ft = { "markdown" },
 }
