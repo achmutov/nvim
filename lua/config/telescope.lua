dd33787e996765802363972e9101dd6f408fee8c
Andrew Chmutov <achmutov@proton.me>
2025-11-04 18:30:29 +0100

    feat: add telescope resume and history scroll

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,4 +1,15 @@
-require("telescope")
+local actions = require("telescope.actions")
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<M-9>"] = actions.cycle_history_next,
+                ["<M-0>"] = actions.cycle_history_prev,
+            },
+        },
+    },
+})
+
 local builtin = require("telescope.builtin")
 
 -- Files
@@ -10,6 +21,7 @@ vim.keymap.set("n", "<leader>pw", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>pr", builtin.resume, {})
 
 vim.keymap.set("n", "<leader>w", builtin.grep_string, {})
 vim.keymap.set("v", "<leader>w", builtin.grep_string, {})
