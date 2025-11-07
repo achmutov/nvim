dacce63bbbdda3d53f3a51bf035f948405d2773c
Andrew Chmutov <achmutov@proton.me>
2025-11-07 03:01:36 +0100

    feat: blink allow docs show

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -14,6 +14,8 @@ return {
             nerd_font_variant = "mono",
         },
 
+        completion = { documentation = { auto_show = true } },
+
         sources = {
             default = { "lsp", "path", "snippets", "buffer" },
         },
